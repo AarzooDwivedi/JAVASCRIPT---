897fadd8237593f53c010b1dbde9e67b33af736d
AarzooDwivedi <125575396+AarzooDwivedi@users.noreply.github.com>
2024-05-27 04:01:21 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// strict() --> is used in browsers
+
+/* 
+PRIMITIVE:
+
+datatypes are:
+1.number
+2.bigInt
+3.String
+4.Boolean
+5.null 
+6.undefined
+7.Symbols -> unique identifcation
+
+
+NON - PRIMITIVE
+
+objects
+*/
+
+//typeof is used to get the type/datatype of the value or variable
+console.log(typeof "Aarzoo");
+
+let age = 20
+console.log(typeof age);
+
+//impo
+console.log(typeof null); // Object
+console.log(typeof undefined); // uhdefined
