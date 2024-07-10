8302ac1b64baad7e9888a8e402e1ce0ef9ebf8be
AarzooDwivedi <125575396+AarzooDwivedi@users.noreply.github.com>
2024-07-10 17:39:35 +0000

    Comparisions

---
+++ 01_basics/05_comparisons.js
@@ -0,0 +1,36 @@
+console.log( 2>1 );
+console.log( 2>=1 );
+console.log( 2<1 );
+console.log( 2<=1 );
+console.log( 2!=1 );
+
+
+// it is always adviced to have same datatypes
+console.log("2"+1);
+console.log("02"+1);
+
+
+console.log( null > 0 );
+console.log( null == 0 );
+console.log( null >= 0);
+
+console.log( undefined > 0 );
+console.log( undefined == 0 );
+console.log( undefined >= 0 );
+
+// Strict check --> ===
+// basically in this not only value but it also checksthe datatype of the variables or constant
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+
+// *************** Summary *************************
+
+// Primitive
+//  7 types : String , Number, Boolean , null , undefined , symbol, BigInt
+
+
+// Reference (Non Primitive)
+// Array, Objects, Functions
+
